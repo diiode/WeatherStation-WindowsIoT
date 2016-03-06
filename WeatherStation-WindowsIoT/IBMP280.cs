30f9714fce16afc5d32e2920c5a6e154f841a0ff
Bart Dewallef <bartdewallef@hotmail.com>
2016-03-06 21:35:33 +0100

    Add BMP280

---
+++ WeatherStation-WindowsIoT/IBMP280.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherStation_WindowsIoT
+{
+    public interface IBMP280
+    {
+        Task Initialize();
+        Task<float> ReadPressure();
+        Task<float> ReadAltitude(float seaLevel);
+        Task<float> ReadHumidity();
+    }
+}
